74537edccd504ca6415f5b43eae53d65615dfa9e
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2021-05-01 16:36:30 +0530

    UI furnished

--- lib/services/addSong_Firestore.dart
+++ lib/services/addSong_Firestore.dart
@@ -15,17 +15,16 @@ void main() async {
   print('Added song successfully');
 
   //Uncomment Below to add searchkeywords in form of string.
-  currentSong.makestringSearchKeyword('APJR',
-      englishName: 'Aise Palna Jhule Re Rishabh Sambhav Jain RSJ',
-      hindiName: '',
-      originalSong:
-          'Kabir Singh Mere Sohneya | Parampara Thakur & Sachet Tandon',
-      album: '',
-      tirthankar: '',
-      extra1:
-          'महावीर जनम कल्याणक mahavir jayanti mahavir janam kalyanak mahaveer janma kalyanak',
-      extra2: '',
-      extra3: '');
+  currentSong.makestringSearchKeyword('KBMB',
+      englishName:
+          'Kyu Bhatke Mann Bawre Kyu bhatke man bavre Kyu bhatke man baavre',
+      hindiName: 'क्यों भटके मन बावरे क्यों तू रोता है',
+      originalSong: '',
+      album: 'Mujhse Shaadi Karogi Lal Dupatta Udit Narayan & Alka Yagnik',
+      tirthankar: 'Rajiv Vijayvargiya',
+      extra1: 'Bollywood Bhakti',
+      extra2: 'Nakoda Bheruji ka beta hokar dheeraj khota hai',
+      extra3: 'भैरू जी का बेटा होकर धीरज खोता है');
   //पारसनाथ पार्श्वनाथ महावीर दीक्षा शांती नाथ सतव जनम कल्याणक दादा अदीश्वर् mahavir jayanti स्तोत्र નેમિનાથ नेमिनाथ
   //शत्रुंजय shatrunjay पालीताना पालीताणा
   //महावीर जनम कल्याणक mahavir jayanti mahavir janam kalyanak mahaveer janma kalyanak
@@ -33,30 +32,30 @@ void main() async {
 
 class AddSong {
   Map<String, dynamic> currentSongMap = {
-    'code': 'APJR',
-    'album': 'Kabir Singh',
+    'code': 'KBMB',
+    'album': 'Mujhse Shaadi Karogi',
     'aaa': 'valid',
-    'category': 'Bhakti',
-    'genre': 'Latest | Janam Kalyanak',
+    'category': 'Bhakti | Bollywood',
+    'genre': '',
     'gujaratiLyrics': '',
     'language': 'Hindi',
     'likes': 0,
     'lyrics':
-        'रेशम की हाय डोरी चमके जैसे चाँद सितारे\nछोटे से महावीरा की हाय कोई नज़र उतारे\nऐसे पलना झूले रे\nके जैसे कोई चाँद को छुले रे\n\nतीनो लोक में हो जैसे कोई शादी रे\nसारी दुनिया से आये हो बाराती रे\nलाल है आया\nखुशीआं है लाया\nदिल में समाया !!\nआँखें टॉम- टॉम- तमके रे\nहो जैसे सारे तारे चमके रे\nऐसे पलना झूले रे......!!\n\nजैसे अंगना में बाजे शहनाई रे\nआयी ख़ुशी वाली घड़ियाँ आयी रे\nलाल है आया\nखुशियां है लाया\nदिल में समाया !!\nआँखें टॉम - टॉम - तमके रे\nहो जैसे सारे तारे चमके रे\nऐसे पलना झूले रे.....!!!!\n\nछोटे से महावीरा.....!!!\n',
+        'क्यों भटके मन बावरे क्यों तू रोता है\nभैरू जी का बेटा होकर धीरज खोता है..\nतू रख विश्वास ओ प्यारे वह हर पल साथ है प्यारे...\n\nउगता है गर सुबह का सूरज शाम को वह ढल जाता है\nइसी तरह जीवन में भी सुख दुख आता जाता है \nबल मांग प्रभु से जीने का.. सुख दुख के आंसू पीने का\nसुख में हंसता दुख में क्यों तु नयन भिगोता है..\nभैरू जी का बेटा.........\n\nछोड़ दिखावा चकाचौंध का काहे मनवा भरमावे\nना जाने किस रुप में तेरे सामने वह खुद आजावे\nबस नित्य लगा ले तू अर्जी.. और छोड़ प्रभु पर तू मर्जी\nभगवान की मर्जी पर क्यों न राजी होता है.. \nभैरू जी का बेटा.........\n',
     'englishLyrics':
-        'Resham ki haye dori chamke jaise chaand sitaare\nChhote se mahaveera ki haye koi nazar utaare\nAise palna jhule re\nKe jaise koi chaand ko chule re\n\nTeeno lok me ho jaise koi shaadi re\nSaari duniya se aaye ho baraati re\nLaal hai aaya \nKhushiaan hai laaya\nDil me samaaya !!\nAankhein tam- tam- tamke re\nHo jaise saare taare chamke re\nAise palna jhule re......!!\n\nJaise angana me baaje shehnaai re\nAayi khushi waali ghadiyaan aayi re\nLaal hai aaya\nKhushiyaan hai laaya\nDil me samaaya !!\nAankhein tam - tam - tamke re\nHo jaise saare taare chamke re\nAise palna jhule re.....!!!!\n\nChote se mahaveera.....!!!\n',
-    'originalSong': 'Mere Sohneya | Parampara Thakur & Sachet Tandon',
+        'Kyu bhatke man baavre kyu tu rota hai\nBheruji ka beta hokar dheeraj khota hai\nTu rakh vishwas o pyare wah har pal sath hai pyare\n\nUgta hai gar subah ka suraj\nSham ko wah dhal jaata hai.\nIsi tarah jeevan me bhi \nsukh dukh aata jaata hai\nBal mang prabhu se jeene ka.....\n\nSukh dukh ke aasu peene ka\nSukh me hasta dukh me \nkyu tu nayan bhigota hai\nBheruji ka beta...........\n\nChod dikhava chakachaundh \nKa kahe manva bharmave\nNa jaane kis roop me\nTere samne wah khud aa jave\nBas nitya laga le tu arji.......\nAur chod prabhu par tu marji\nBhagwan ki marji par \nKyu na raaji hota hai\nBheruji ka beta...........\n',
+    'originalSong': 'Lal Dupatta | Udit Narayan & Alka Yagnik',
     'popularity': 0,
-    'production': 'Bhakti Bhavna',
+    'production': 'Rajiv Vijayvargiya',
     'share': 0,
-    'singer': 'Rishabh Sambhav Jain (RSJ)',
-    'songNameEnglish': 'Aise Palna Jhule Re',
-    'songNameHindi': '',
-    'tirthankar': 'Mahavir Swami',
+    'singer': 'Rajiv Vijayvargiya',
+    'songNameEnglish': 'Kyu Bhatke Mann Bawre',
+    'songNameHindi': 'क्यों भटके मन बावरे',
+    'tirthankar': 'Nakoda Bheruji',
     'totalClicks': 0,
     'todayClicks': 0,
     'trendPoints': 0.0,
-    'youTubeLink': 'https://youtu.be/KaAP2uYBSn0',
+    'youTubeLink': 'https://youtu.be/fS-rUYZ6IiM',
   };
   CollectionReference songs = FirebaseFirestore.instance.collection('songs');
 
